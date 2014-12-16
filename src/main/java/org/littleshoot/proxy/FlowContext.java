8568f0d98693a4f48aa9c9301787f026350df05a
Ivan De Marino <idemarino@newrelic.com>
2014-12-16 15:45:52 +0000

    Revert "Storing (ClientToProxy|ProxyToServer)Connection in Flows"
    This reverts commit 843b2f33a632eeeca9d3fe166d51c912c3ba5b4e.

--- src/main/java/org/littleshoot/proxy/FlowContext.java
+++ src/main/java/org/littleshoot/proxy/FlowContext.java
@@ -1,58 +1,47 @@
 package org.littleshoot.proxy;
 
-import org.littleshoot.proxy.impl.ClientToProxyConnection;
-
-import javax.net.ssl.SSLSession;
-import java.lang.ref.WeakReference;
 import java.net.InetSocketAddress;
 
+import javax.net.ssl.SSLEngine;
+import javax.net.ssl.SSLSession;
+
+import org.littleshoot.proxy.impl.ClientToProxyConnection;
+
 /**
+ * <p>
  * Encapsulates contextual information for flow information that's being
  * reported to a {@link ActivityTracker}.
- * <p/>
- * NOTE: <code>WeakReference<ProxyToServerConnection></code> is used to hold
- * flow information. This means that, when all strong references to this object
- * are gone, successive calls to this objects might return <code>null</code>.
+ * </p>
  */
 public class FlowContext {
-    private final WeakReference<ClientToProxyConnection> clientToProxyConnection;
+    private final InetSocketAddress clientAddress;
+    private final SSLSession clientSslSession;
 
-    public FlowContext(ClientToProxyConnection clientToProxyConnection) {
-        this.clientToProxyConnection =
-                new WeakReference<ClientToProxyConnection>(
-                        clientToProxyConnection);
+    public FlowContext(ClientToProxyConnection clientConnection) {
+        super();
+        this.clientAddress = clientConnection.getClientAddress();
+        SSLEngine sslEngine = clientConnection.getSslEngine();
+        this.clientSslSession = sslEngine != null ? sslEngine.getSession()
+                : null;
     }
 
     /**
      * The address of the client.
-     *
+     * 
      * @return
      */
     public InetSocketAddress getClientAddress() {
-        ClientToProxyConnection connection = this.clientToProxyConnection.get();
-        return null == connection ? null : connection.getClientAddress();
+        return clientAddress;
     }
 
     /**
      * If using SSL, this returns the {@link SSLSession} on the client
      * connection.
-     *
+     * 
      * @return
      */
     public SSLSession getClientSslSession() {
-        ClientToProxyConnection connection = this.clientToProxyConnection.get();
-        if (null != connection && null != connection.getSslEngine()) {
-            return connection.getSslEngine().getSession();
-        }
-        return null;
+        return clientSslSession;
     }
 
-    /**
-     * The ClientToProxyConnection associated with this comms.
-     *
-     * @return
-     */
-    public ClientToProxyConnection getClientToProxyConnection() {
-        return clientToProxyConnection.get();
-    }
 }
--- src/main/java/org/littleshoot/proxy/FullFlowContext.java
+++ src/main/java/org/littleshoot/proxy/FullFlowContext.java
@@ -3,51 +3,37 @@ package org.littleshoot.proxy;
 import org.littleshoot.proxy.impl.ClientToProxyConnection;
 import org.littleshoot.proxy.impl.ProxyToServerConnection;
 
-import java.lang.ref.WeakReference;
-
 /**
  * Extension of {@link FlowContext} that provides additional information (which
  * we know after actually processing the request from the client).
- * <p/>
- * NOTE: <code>WeakReference<ProxyToServerConnection></code> is used to hold
- * flow information. This means that, when all strong references to this object
- * are gone, successive calls to this objects might return <code>null</code>.
  */
 public class FullFlowContext extends FlowContext {
-    private final WeakReference<ProxyToServerConnection> proxyToServerConnection;
+    private final String serverHostAndPort;
+    private final ChainedProxy chainedProxy;
 
     public FullFlowContext(ClientToProxyConnection clientConnection,
-                           ProxyToServerConnection proxyToServerConnection) {
+            ProxyToServerConnection serverConnection) {
         super(clientConnection);
-        this.proxyToServerConnection = new WeakReference<ProxyToServerConnection>(proxyToServerConnection);
+        this.serverHostAndPort = serverConnection.getServerHostAndPort();
+        this.chainedProxy = serverConnection.getChainedProxy();
     }
 
     /**
      * The host and port for the server (i.e. the ultimate endpoint).
-     *
+     * 
      * @return
      */
     public String getServerHostAndPort() {
-        ProxyToServerConnection connection = proxyToServerConnection.get();
-        return null == connection ? null : connection.getServerHostAndPort();
+        return serverHostAndPort;
     }
 
     /**
      * The chained proxy (if proxy chaining).
-     *
+     * 
      * @return
      */
     public ChainedProxy getChainedProxy() {
-        ProxyToServerConnection connection = proxyToServerConnection.get();
-        return null == connection ? null : connection.getChainedProxy();
+        return chainedProxy;
     }
 
-    /**
-     * The ProxyToServer connection associated with this comms.
-     *
-     * @return
-     */
-    public ProxyToServerConnection getProxyToServerConnection() {
-        return proxyToServerConnection.get();
-    }
 }
